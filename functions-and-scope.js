3c7415c0f2016902b1df09e16497f7584e1a2d0a
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-03-24 16:03:19 +0100

    Add opdracht 1

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -1,7 +1,7 @@
 // Je gaat functies schrijven die we kunnen hergebruiken om een lijst met eindcijfers van studenten te checken. Je zult over de cijfers heen moeten itereren (hoe pak je dat aan?),
 // maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
-
+console.log('-------------Opdracht 1-------------');
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 /* Opdracht  1: Cum Laude */
@@ -15,7 +15,14 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
+let amountOfCumLaude = 0;
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] === 8 || grades[i] > 8) {
+        amountOfCumLaude += 1;
+    }
 
+}
+console.log(amountOfCumLaude);
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
